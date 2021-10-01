e8e98c9d937492cf923f6dc6ab31f28092113394
chaeeun <1005honeybadger@gmail.com>
2021-10-01 23:50:56 +0900

    codeup 01_print

---
+++ 00_codeup/01_print.py
@@ -0,0 +1,24 @@
+# 6001~6008 : print
+# 1
+print("Hello")
+
+# 2
+print("Hello World")
+
+# 3
+print('Hello World', sep="\n")
+
+# 4
+print("\'Hello\'")
+
+# 5
+print("\"Hello World\"")
+
+# 6
+print('\"!@#$%^&*()\'')
+
+# 7
+print('\"C:\\Download\\\'hello\'.py\"')
+
+# 8
+print('print(\"Hello\\nWorld\")')
