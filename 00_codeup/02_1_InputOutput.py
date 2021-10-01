8e1e6bc1942e732d6200526b71616fe0659ec2c8
chaeeun <1005honeybadger@gmail.com>
2021-10-01 23:58:46 +0900

    codeup I/O 1

---
+++ 00_codeup/02_1_InputOutput.py
@@ -0,0 +1,33 @@
+# 6009
+str1 = input()
+print(str1)
+
+# 6010
+a = int(input())
+print(a)
+
+# 6011
+a = float(input())
+print(a)
+
+# 6012
+a = input()
+b = input()
+print(a, b, sep='\n')
+
+# 6013
+a = input()
+b = input()
+print(b, a, sep="\n")
+
+# 6014
+a = float(input())
+print(a, a, a, sep="\n")
+
+# 6015
+a, b = input().split()
+print(a, b, sep="\n")
+
+# 6016
+a, b = input().split()
+print(b, a)
