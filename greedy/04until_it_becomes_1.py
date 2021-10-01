0b1fae4b66c9585120902aedf77290bcb64cdc87
chaeeun <1005honeybadger@gmail.com>
2021-10-01 19:16:29 +0900

    perfect problem explanation

--- greedy/04until_it_becomes_1.py
+++ greedy/04until_it_becomes_1.py
@@ -16,15 +16,16 @@ count = 0
 # print(count)
 
 #문제해설(단순)
-while n >= k:
-    while n % k != 0:
-        n-=1
-        count += 1
-    n // k
-    count += 1
+while True:
+    target = (n//k) * k # k로 나누어 떨어지는 수가 될때까지 -1
+    count += (n-target) 
+    n = target
 
-while n > 1:
-    n -= 1
+    if n < k:
+        break
+    
     count += 1
+    n //= k
 
-print(count) 
+count += (n-1)
+print(count)
