7d3be7dfa0e47b5fbf42f4320bcaee8678296e38
chaeeun <1005honeybadger@gmail.com>
2021-10-01 19:01:00 +0900

    simple problem explanation

--- greedy/04until_it_becomes_1.py
+++ greedy/04until_it_becomes_1.py
@@ -1,16 +1,30 @@
 n, k = map(int, input().split())
 
 count = 0
-while True:    
-    if(n % k == 0):
-        n //= k
-        count += 1
-        if(n == 1):
-            break
-    if(n == 1):
-        break
-    if(n % k != 0):
-        n -= 1
-        count += 1
+# while True:    
+#     if(n % k == 0):
+#         n //= k
+#         count += 1
+#         if(n == 1):
+#             break
+#     if(n == 1):
+#         break
+#     if(n % k != 0):
+#         n -= 1
+#         count += 1
 
-print(count)
+# print(count)
+
+#문제해설(단순)
+while n >= k:
+    while n % k != 0:
+        n-=1
+        count += 1
+    n // k
+    count += 1
+
+while n > 1:
+    n -= 1
+    count += 1
+
+print(count) 
