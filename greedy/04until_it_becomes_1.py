763e62833d4714aa4d1c98f43bf27b60c92d77db
chaeeun <1005honeybadger@gmail.com>
2021-10-01 18:52:09 +0900

    solve

---
+++ greedy/04until_it_becomes_1.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+
+count = 0
+while True:    
+    if(n % k == 0):
+        n //= k
+        count += 1
+        if(n == 1):
+            break
+    if(n == 1):
+        break
+    if(n % k != 0):
+        n -= 1
+        count += 1
+
+print(count)
