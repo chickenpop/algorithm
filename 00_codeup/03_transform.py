0cd6971bcdecb2528b81be42466de0e0d12d6463
chaeeun <1005honeybadger@gmail.com>
2021-10-02 23:06:08 +0900

    codeup 100 25-31 transform

---
+++ 00_codeup/03_transform.py
@@ -0,0 +1,28 @@
+# 6025
+a, b = map(int, input().split())
+print(a+b)
+
+# 6026
+float1 = float(input())
+float2 = float(input())
+print(float1+float2)
+
+# 6027
+num = int(input())
+print("%x" % num)
+
+# 6028
+num = int(input())
+print("%X" % num)
+
+# 6029
+num = int(input(), 16)
+print("%o" % num)
+
+# 6030
+unicode = ord(input())
+print(unicode)
+
+# 6031
+unicode_num = chr(int(input()))
+print(unicode_num)
