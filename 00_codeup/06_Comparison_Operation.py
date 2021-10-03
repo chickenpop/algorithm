c6021f85bea45d38516816ca251055a38130908a
chaeeun <1005honeybadger@gmail.com>
2021-10-03 15:09:35 +0900

    codeup 48-51 Comparison Operation

---
+++ 00_codeup/06_Comparison_Operation.py
@@ -0,0 +1,15 @@
+# 6048 a<b True?
+num1, num2 = map(int, input().split())
+print(num1<num2)
+
+# 6049 a==b True?
+num1, num2 = map(int, input().split())
+print(num1==num2)
+
+# 6050 a<=b True?
+num1, num2 = map(int, input().split())
+print(num1<=num2)
+
+# 6051 a!=b True?
+num1, num2 = map(int, input().split())
+print(num1!=num2)
