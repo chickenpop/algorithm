91cb720c0fb72be5ae01c0a5b7beaa9ec440b39b
chaeeun <1005honeybadger@gmail.com>
2021-10-03 12:55:15 +0900

    codeup 32-38 Arithmetic Operators

---
+++ 00_codeup/04_1_Arithmetic_Operators.py
@@ -0,0 +1,28 @@
+# 6032
+n = -(int(input()))
+print(n)
+
+# 6033
+chr1 = ord(input())+1
+print(chr(chr1))
+
+# 6034
+a, b = map(int, input().split())
+print(a-b)
+
+# 6035
+num1, num2 = map(float, input().split())
+print(num1*num2)
+
+# 6036
+w, n = input().split()
+print(w*int(n))
+
+# 6037
+n = input()
+s = input()
+print(int(n)*s)
+
+# 6038
+num1, num2 = map(int, input().split())
+print(num1**num2)
