375ab1cc3100b1121e3e46dc76f4cf67f5020209
chaeeun <1005honeybadger@gmail.com>
2021-10-05 23:03:36 +0900

    codeup 92-94 list

--- 00_codeup/14_list.py
+++ 00_codeup/14_list.py
@@ -1,16 +1,16 @@
 # 6092
-# n = int(input())
-# num = list(map(int, input().split()))
-# d = []
+n = int(input())
+num = list(map(int, input().split()))
+d = []
 
-# for i in range(23):
-#     d.append(0) 
+for i in range(23):
+    d.append(0) 
 
-# for j in range(n):
-#     d[num[j]-1] += 1
+for j in range(n):
+    d[num[j]-1] += 1
 
-# for k in range(23):
-#     print(d[k], end=" ")
+for k in range(23):
+    print(d[k], end=" ")
 
 # 6093
 n = int(input())
@@ -18,3 +18,18 @@ k =list(map(int, input().split()))
 
 for i in range(n-1, -1, -1):
     print(k[i], end=" ")
+
+# 6094
+n = int(input())
+k = list(map(int, input().split()))
+k.sort()
+print(k[0])
+#or
+n = int(input())
+k = list(map(int, input().split()))
+min = k[0]
+for i in range(n):
+    if min >= k[i]:
+        min = k[i]
+
+print(min)
