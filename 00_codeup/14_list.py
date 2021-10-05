031fdd160af8b7e1386077c9defafeca91189269
chaeeun <1005honeybadger@gmail.com>
2021-10-05 22:53:07 +0900

    codeup 93

--- 00_codeup/14_list.py
+++ 00_codeup/14_list.py
@@ -1,13 +1,20 @@
 # 6092
+# n = int(input())
+# num = list(map(int, input().split()))
+# d = []
+
+# for i in range(23):
+#     d.append(0) 
+
+# for j in range(n):
+#     d[num[j]-1] += 1
+
+# for k in range(23):
+#     print(d[k], end=" ")
+
+# 6093
 n = int(input())
-num = list(map(int, input().split()))
-d = []
+k =list(map(int, input().split()))
 
-for i in range(23):
-    d.append(0) 
-
-for j in range(n):
-    d[num[j]-1] += 1
-
-for k in range(23):
-    print(d[k], end=" ")
+for i in range(n-1, -1, -1):
+    print(k[i], end=" ")
