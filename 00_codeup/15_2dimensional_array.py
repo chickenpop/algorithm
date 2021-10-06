f21951f78b79300e0b4568a91a6104d09a486fae
chaeeun <1005honeybadger@gmail.com>
2021-10-06 19:39:04 +0900

    codeup 97

--- 00_codeup/15_2dimensional_array.py
+++ 00_codeup/15_2dimensional_array.py
@@ -35,3 +35,21 @@
 #     for k in range(19):
 #         print(d[l][k], end=" ")
 #     print()
+
+# 6097 가장 많이 틀린 문제
+w, h = map(int, input().split())
+k = [[0 for _ in range(h)] for _ in range(w)] 
+
+n = int(input())
+for i in range(n):
+    l, d, x, y = map(int, input().split())
+    for L in range(l):
+        if d == 0:
+            k[x-1][y-1+L] = 1
+        elif d == 1 :
+            k[x-1+L][y-1] = 1
+
+for m in range(w):
+    for j in range(h):
+        print(k[m][j], end=" ")
+    print(end="\n")
