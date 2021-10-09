eb889dadbe8f055ea01fd376f488b4bd5eb164a2
chaeeun <1005honeybadger@gmail.com>
2021-10-09 18:54:42 +0900

    codeup 1461

--- 00_codeup/etc/01list_training.py
+++ 00_codeup/etc/01list_training.py
@@ -8,3 +8,19 @@ for i in range(n):
         count+=1
         print(count, end=" ")
     print() 
+
+# 1461
+
+n = int(input())
+cnt = 0
+d = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(len(d)):
+    for j in range(len(d[i]) - 1, -1, -1):
+        cnt += 1
+        d[i][j] = cnt
+
+for i in d:
+    print(*i, " ")
+
+# 1462
