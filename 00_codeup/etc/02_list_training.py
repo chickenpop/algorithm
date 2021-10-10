ae65029e96c406e1145caa07ea66455479200681
chaeeun <1005honeybadger@gmail.com>
2021-10-10 16:12:35 +0900

    codeup 1470

--- 00_codeup/etc/02_list_training.py
+++ 00_codeup/etc/02_list_training.py
@@ -16,19 +16,37 @@
 #     print(*i, " ")
 
 # 1469   
+# n = int(input())
+# d = [[0 for _ in range(n)] for _ in range(n)]
+# cnt = 0
+
+# for i in range(n):
+#     if i % 2 == 0:
+#         for j in range(n-1, -1, -1):
+#             cnt += 1
+#             d[i][j] = cnt
+#     else : 
+#         for j in range(n):
+#             cnt += 1
+#             d[i][j] = cnt
+
+# for i in d:
+#     print(*i, " ")
+
+# 1470 
 n = int(input())
 d = [[0 for _ in range(n)] for _ in range(n)]
 cnt = 0
 
 for i in range(n):
     if i % 2 == 0:
-        for j in range(n-1, -1, -1):
-            cnt += 1
-            d[i][j] = cnt
-    else : 
         for j in range(n):
             cnt += 1
-            d[i][j] = cnt
+            d[j][i] = cnt
+    else: 
+        for j in range(n-1, -1, -1):
+            cnt += 1
+            d[j][i] = cnt
 
 for i in d:
     print(*i, " ")
