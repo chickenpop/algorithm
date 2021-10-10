cca31cffcefae746c0826e1e8d5d1658dd355c15
chaeeun <1005honeybadger@gmail.com>
2021-10-10 16:38:45 +0900

    codeup 1474

--- 00_codeup/etc/02_list_training.py
+++ 00_codeup/etc/02_list_training.py
@@ -69,32 +69,51 @@
 # for i in d:
 #     print(*i, " ")
 
-# 1472
+# # 1472
+# n, m = map(int, input().split())
+# d = [[0 for _ in range(m)] for _ in range(n)]
+# cnt = 0
+
+# if n % 2 == 0:
+#     for i in range(n-1, -1, -1):
+#         if i % 2 == 0:
+#             for j in range(m):
+#                 cnt += 1
+#                 d[i][j] = cnt
+#         else :
+#             for j in range(m-1, -1, -1):
+#                 cnt += 1
+#                 d[i][j] = cnt
+# else :
+#     for i in range(n-1, -1, -1):
+#         if i % 2 == 1:
+#             for j in range(m):
+#                 cnt += 1
+#                 d[i][j] = cnt
+#         else :
+#             for j in range(m-1, -1, -1):
+#                 cnt += 1
+#                 d[i][j] = cnt
+
+
+# for i in d:
+#     print(*i, " ")
+
+# 1473
 n, m = map(int, input().split())
 d = [[0 for _ in range(m)] for _ in range(n)]
 cnt = 0
 
-if n % 2 == 0:
-    for i in range(n-1, -1, -1):
-        if i % 2 == 0:
-            for j in range(m):
-                cnt += 1
-                d[i][j] = cnt
-        else :
-            for j in range(m-1, -1, -1):
-                cnt += 1
-                d[i][j] = cnt
-else :
-    for i in range(n-1, -1, -1):
-        if i % 2 == 1:
-            for j in range(m):
-                cnt += 1
-                d[i][j] = cnt
-        else :
-            for j in range(m-1, -1, -1):
-                cnt += 1
-                d[i][j] = cnt
-
+for i in range(n):
+    idx = n - i - 1
+    if i % 2 == 0:
+        for j in range(m):
+            cnt += 1
+            d[idx][j] = cnt
+    else :
+        for j in range(m-1, -1, -1):
+            cnt += 1
+            d[idx][j] = cnt
 
 for i in d:
-    print(*i, " ")
+    print(*i," ")
