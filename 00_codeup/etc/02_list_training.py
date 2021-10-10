e8c3049143e3486f7bce46c97caf7953abb3d7c8
chaeeun <1005honeybadger@gmail.com>
2021-10-10 18:57:22 +0900

    codeup 1474

--- 00_codeup/etc/02_list_training.py
+++ 00_codeup/etc/02_list_training.py
@@ -100,20 +100,42 @@
 #     print(*i, " ")
 
 # 1473
+# n, m = map(int, input().split())
+# d = [[0 for _ in range(m)] for _ in range(n)]
+# cnt = 0
+
+# for i in range(n):
+#     idx = n - i - 1
+#     if i % 2 == 0:
+#         for j in range(m):
+#             cnt += 1
+#             d[idx][j] = cnt
+#     else :
+#         for j in range(m-1, -1, -1):
+#             cnt += 1
+#             d[idx][j] = cnt
+
+# for i in d:
+#     print(*i," ")
+
+# 1474
 n, m = map(int, input().split())
 d = [[0 for _ in range(m)] for _ in range(n)]
 cnt = 0
+k = 0
 
-for i in range(n):
-    idx = n - i - 1
-    if i % 2 == 0:
-        for j in range(m):
+for j in range(m-1, -1, -1):
+    if k % 2 == 1:
+        for i in range(n):
             cnt += 1
-            d[idx][j] = cnt
+            d[i][j] = cnt
+        k += 1
     else :
-        for j in range(m-1, -1, -1):
+        for i in range(n-1, -1, -1):
             cnt += 1
-            d[idx][j] = cnt
+            d[i][j] = cnt
+        k += 1
+
 
 for i in d:
     print(*i," ")
