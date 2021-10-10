0e28b16123486c44ab9fe2526e5acf7de4d6f2c5
chaeeun <1005honeybadger@gmail.com>
2021-10-10 16:18:36 +0900

    codeup 1471

--- 00_codeup/etc/02_list_training.py
+++ 00_codeup/etc/02_list_training.py
@@ -34,12 +34,30 @@
 #     print(*i, " ")
 
 # 1470 
+# n = int(input())
+# d = [[0 for _ in range(n)] for _ in range(n)]
+# cnt = 0
+
+# for i in range(n):
+#     if i % 2 == 0:
+#         for j in range(n):
+#             cnt += 1
+#             d[j][i] = cnt
+#     else: 
+#         for j in range(n-1, -1, -1):
+#             cnt += 1
+#             d[j][i] = cnt
+
+# for i in d:
+#     print(*i, " ")
+
+# 1471
 n = int(input())
 d = [[0 for _ in range(n)] for _ in range(n)]
 cnt = 0
 
 for i in range(n):
-    if i % 2 == 0:
+    if i % 2 == 1:
         for j in range(n):
             cnt += 1
             d[j][i] = cnt
