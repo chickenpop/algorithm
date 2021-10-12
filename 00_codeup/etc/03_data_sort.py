2b4f703e7ec27efd7fefdebde447b3dced4c06af
chaeeun <1005honeybadger@gmail.com>
2021-10-12 22:36:27 +0900

    codeup 3011 & (1441, 1442  revise)

--- 00_codeup/etc/03_data_sort.py
+++ 00_codeup/etc/03_data_sort.py
@@ -5,7 +5,7 @@
 
 # print(*num, sep=" ")
 
-# 1420
+# # 1420
 # n = int(input())
 # d = {}
 
@@ -17,7 +17,11 @@
 
 # print(data[2][0])
 
-# 1441
+''' 
+문제의 해결방향과 다른 풀이 방법
+밑에 경우는 오름차순 정렬에 관련된 방법
+
+# 1441 
 n = int(input())
 d = [0 for _ in range(n)]
 
@@ -28,3 +32,34 @@ d.sort()
 
 for i in range(n):
     print(d[i])
+
+# 1442
+n = int(input())
+d = [0 for _ in range(n)]
+data = []
+
+for i in range(n):
+    d[i] = int(input())
+    
+data = sorted(d, reverse = False)
+
+for i in range(n):
+    print(data[i])
+
+'''
+
+# 3011
+n = int(input())
+d = list(map(int, input().strip().split()))
+cnt = 0
+
+for i in range(n):
+    swapped = False
+    for j in range(n-i-1):
+        if(d[j] > d[j+1]):
+            d[j], d[j+1] = d[j+1], d[j]
+            swapped = True
+    if swapped:
+        cnt+=1
+
+print(cnt)
