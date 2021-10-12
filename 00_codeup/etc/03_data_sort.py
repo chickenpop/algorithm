b59b547f87b9d37e559665f827c593793069aa36
chaeeun <1005honeybadger@gmail.com>
2021-10-12 23:02:46 +0900

    codeup 1172, 1420, 3011 data sort

--- 00_codeup/etc/03_data_sort.py
+++ 00_codeup/etc/03_data_sort.py
@@ -1,21 +1,21 @@
-# # 1172
-# num = list(map(int, input().split()))
+# 1172
+num = list(map(int, input().split()))
 
-# num.sort()
+num.sort()
 
-# print(*num, sep=" ")
+print(*num, sep=" ")
 
-# # 1420
-# n = int(input())
-# d = {}
+# 1420
+n = int(input())
+d = {}
 
-# for i in range(n):
-#     name, score = input().split()
-#     d[name] = int(score)
+for i in range(n):
+    name, score = input().split()
+    d[name] = int(score)
 
-# data = sorted(d.items(), key= lambda t:t[1], reverse=True)
+data = sorted(d.items(), key= lambda t:t[1], reverse=True)
 
-# print(data[2][0])
+print(data[2][0])
 
 ''' 
 문제의 해결방향과 다른 풀이 방법
@@ -50,7 +50,7 @@ for i in range(n):
 
 # 3011
 n = int(input())
-d = list(map(int, input().strip().split()))
+d = list(map(int, input().strip().split())) # strip() 전달된 문자의 왼쪽, 오른쪽 공백을 제거함
 cnt = 0
 
 for i in range(n):
@@ -63,3 +63,29 @@ for i in range(n):
         cnt+=1
 
 print(cnt)
+
+# 버블 정렬 이해를 위한 문제연습
+n = int(input())
+d = list(map(int, input().strip().split())) # strip() 전달된 문자의 왼쪽, 오른쪽 공백을 제거함
+cnt = 0
+
+# for i in range(n-1, 0, -1):
+#     swapped = False
+#     for j in range(i):
+#         if(d[j] > d[j+1]):
+#             d[j], d[j+1] = d[j+1], d[j]
+#             swapped = True
+#     if not swapped:
+#         break
+
+# 코드업에서 72~79 코드와 82~89 코드 사이에 유효한 시간 차는 없음 (약 8)
+end = n - 1
+while end > 0:
+    last_swap = 0
+    for i in range(end):
+        if d[i] > d[i + 1]:
+            d[i], d[i + 1] = d[i + 1], d[i]
+            last_swap = i
+    end = last_swap
+
+print(*d, sep=" ")
