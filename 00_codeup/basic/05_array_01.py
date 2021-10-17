84b85f97ae18eb8e01913eb524d87afe7b649a3d
chaeeun <1005honeybadger@gmail.com>
2021-10-17 23:46:59 +0900

    codeup 1402-05

--- 00_codeup/basic/05_array_01.py
+++ 00_codeup/basic/05_array_01.py
@@ -8,3 +8,25 @@ data = sorted(d, reverse=True)
 
 for i in range(n):
     print(data[i], end=" ")
+
+# 1403
+n = int(input())
+d = [0 for _ in range(n)]
+
+d = list(map(int, input().split()))
+cnt = 0
+
+for i in range(2):
+    for j in range(n):
+        print(d[j])
+
+# 1405
+n = int(input())
+d = [0 for _ in range(n)]
+k = 0
+d = list(map(int, input().split()))
+
+for i in range(n):
+    for j in range(n):
+        print(d[i+j-n], end=" ")
+    print()
