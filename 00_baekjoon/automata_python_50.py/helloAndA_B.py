5a1794a6c91a0468ca1d0c269f4cfde55deb7e90
chaeeun <1005honeybadger@gmail.com>
2021-10-29 17:24:28 +0900

    baekjoon python training

---
+++ 00_baekjoon/automata_python_50.py/helloAndA_B.py
@@ -0,0 +1,40 @@
+2557
+print("Hello World!")
+
+#1000
+a, b = map(int, input().split())
+print(a+b)
+
+#10998
+a, b = map(int,input().split())
+print(a*b)
+
+#1001
+a, b = map(int,input().split())
+print(a-b)
+
+#1008 
+a, b = map(int,input().split())
+print("%.9f"%(a/b))
+
+#10869
+a, b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+#10430
+a, b, c = map(int,input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+#2558
+a = int(input())
+b = int(input())
+print(a+b)
+
+#2588
