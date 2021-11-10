ccb05514f2e347ffdb45b269cd4a1b75f1484b98
chaeeun <1005honeybadger@gmail.com>
2021-11-10 20:05:46 +0900

    baekjoon greedy update 1946

--- 00_baekjoon/greedy/07_NewPerson.py
+++ 00_baekjoon/greedy/07_NewPerson.py
@@ -1,11 +1,16 @@
 #1946 신입사원
+import sys
 Testcase = int(input())
 
 for i in range(Testcase):
     n = int(input())
     doc_interview = []
     for j in range(n):
-        document, interview = list(map(int, input().split()))
+        document, interview = map(int,sys.stdin.readline().split())
+        '''
+        document, interview = map(int, input().split())
+        기존 방식으로 입력을 받으면 시간초과로 인해 실패한다
+        '''
         doc_interview.append([document, interview])
         
     doc_interview.sort()
