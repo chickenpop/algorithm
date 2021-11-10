3cacd10758fb86ca202d12480c243505dcbfcfaf
chaeeun <1005honeybadger@gmail.com>
2021-11-10 19:53:24 +0900

    baekjoon greedy update 1026

---
+++ 00_baekjoon/greedy/06_Treasure.py
@@ -0,0 +1,13 @@
+#1026 보물
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+sum = 0
+
+for i in range(n):
+    sum += A[i]*B[i]
+
+print(sum)
