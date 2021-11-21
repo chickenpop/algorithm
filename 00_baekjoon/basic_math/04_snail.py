f9621289911d9838db74b6dcf9879205470cffb7
chaeeun <1005honeybadger@gmail.com>
2021-11-21 22:35:53 +0900

    baekjoon basicmath update 2869

--- 00_baekjoon/basic_math/04_snail.py
+++ 00_baekjoon/basic_math/04_snail.py
@@ -1,15 +1,9 @@
 # 2869 달팽이는 올라가고 싶다
 import sys
+import math
 a, b, v = map(int,sys.stdin.readline().split())
-v_sum = 0 # 달팽이 위치
-cnt = 0   # 걸리는 일수
+days = 0   # 걸리는 일수
 
-while v_sum <= v:
-    cnt += 1
-    v_sum += a
-    if v_sum >= v:
-        break
-    else:
-        v_sum -= b
+days = math.ceil((v-a)/(a-b)) + 1
 
-print(cnt)
+print(days)
