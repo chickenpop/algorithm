96b2cae87203938e7ca5bbfd8779fc881b9d97ac
chaeeun <1005honeybadger@gmail.com>
2021-11-24 18:00:36 +0900

    baekjoon basic math 2839 update

--- 00_baekjoon/basic_math/07_SugarDelivery.py
+++ 00_baekjoon/basic_math/07_SugarDelivery.py
@@ -1,14 +1,17 @@
 # 2839 설탕 배달
 n = int(input())
-cnt = 0
+cnt = 0   # 배달되는 설탕 봉지 수
+error = 0 # 출력 초과를 방지하기 위한 판단 변수
 
-while True:
+while error < 100:
     if n % 5 == 0:
         cnt += n//5
         print(cnt)
         break
     elif n < 0:
         print(-1)
+        break
     else:
         n -= 3
-        cnt += 1 
+        cnt += 1
+        error += 1
