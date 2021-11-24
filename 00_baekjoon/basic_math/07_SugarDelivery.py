7fd979f8156bae3c4d2403709cc4dd8bed34643c
chaeeun <1005honeybadger@gmail.com>
2021-11-24 17:53:09 +0900

    baekjoon 2839 fail over output

--- 00_baekjoon/basic_math/07_SugarDelivery.py
+++ 00_baekjoon/basic_math/07_SugarDelivery.py
@@ -2,29 +2,13 @@
 n = int(input())
 cnt = 0
 
-n = int(input())
-cnt = 0
-
 while True:
     if n % 5 == 0:
         cnt += n//5
         print(cnt)
         break
-    elif (n%5)%3 == 0:
-        cnt += n//5 + (n%5)//3
-        print(cnt)
-        break
-    elif (n%3)%5 == 0:
-        cnt += n//3 + (n%3)//5
-        print(cnt)
-        break
-    elif n % 3 == 0:
-        cnt += n//3
-        print(cnt)
-        break
-    elif n < 0 :
+    elif n < 0:
         print(-1)
-        break
-
-    n -= 3
-    cnt += 1 
+    else:
+        n -= 3
+        cnt += 1 
