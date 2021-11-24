0c3817a966429f833ae23ed0d624afde6b033b6e
chaeeun <1005honeybadger@gmail.com>
2021-11-24 21:03:00 +0900

    beakjoon basic math 1011 update

--- 00_baekjoon/basic_math/08_FlymetotheAlphaCentauri.py
+++ 00_baekjoon/basic_math/08_FlymetotheAlphaCentauri.py
@@ -1,20 +1,18 @@
 # 1011 Fly me to the Alpha Centauri
-# 시간초과
 import sys
-
-TestCase = int(input())
+import math
+TestCase = int(sys.stdin.readline())
 
 for i in range(TestCase):
     x, y = map(int, sys.stdin.readline().split())
     move = y-x
-    n = 1
-    while True:
-        if 2*n >= move:
-            if n*n >= move:
-                print(n+1)
-                break
-            elif n*(n+1) >= move:
-                print(n+2)
-                break
+    if move <= 3:
+        print(move)
+    else: 
+        n = int(math.sqrt(move))
+        if n**2 == move:
+            print(2*n-1)
+        elif n**2 < move <= n**2+n:
+            print(2*n)
         else:
-            n += 1
+            print(2*n+1)
