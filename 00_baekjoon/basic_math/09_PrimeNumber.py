708d1f81d8fbf1efe64e1a49b6360ddbb00a56c9
chaeeun <1005honeybadger@gmail.com>
2021-11-26 20:10:50 +0900

    baekjoon basic math2 1978 update

--- 00_baekjoon/basic_math/09_PrimeNumber.py
+++ 00_baekjoon/basic_math/09_PrimeNumber.py
@@ -2,17 +2,23 @@
 import math
 
 def PrimeNumber(n):
-    for i in range(2, int(math.sqrt(n))):
-        if n%i==0:
-            return False
-    return True
+    # 예외 사항 if n==1, n==2
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for i in range(2, int(math.sqrt(n))+1):
+            if n%i==0:      # 2~제곱근까지의 수가 n(입력)과 나눠지면 소수가 아니다
+                return False
+    return True             # 소수
 
 n = int(input()) # 소수 갯수
 
 lst = list(map(int, input().split()))
 cnt = 0
 for i in range(len(lst)):
-    if PrimeNumber(lst[i])== True:
+    if PrimeNumber(lst[i]) == True:
         cnt += 1
     else:
         cnt += 0
