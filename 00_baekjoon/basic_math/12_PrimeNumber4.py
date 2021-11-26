c24b95073093f4230b1a31b6c00f2f86dc23e8c1
chaeeun <1005honeybadger@gmail.com>
2021-11-26 23:20:06 +0900

    baekjoon basic math2 4948 update

--- 00_baekjoon/basic_math/12_PrimeNumber4.py
+++ 00_baekjoon/basic_math/12_PrimeNumber4.py
@@ -1,22 +1,31 @@
 # 4948 베르트랑 공준
 import math
+import sys
 
 
 def PrimeNumber(num):
-    for i in range(2, int(math.sqrt(num)+1)):
-        if num % i == 0:
-            return False
-    return True
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(num)+1)):
+            if num % i == 0:
+                return False
+        return True
 
 
-while True:
-    n = int(input())
-    if n == 0:
-        break
+all_lst = list(range(2, 246912))  # 지정 범위만큼 리스트 생성
+save_lst = []
+
+for i in all_lst:
+    if PrimeNumber(i) == True:
+        save_lst.append(i)
+
+n = int(sys.stdin.readline())
+
+while n != 0:
     cnt = 0
-    for i in range(n, 2*n+1):
-        if i == 1:
-            continue
-        elif PrimeNumber(i) == True:
+    for i in save_lst:
+        if n < i <= 2*n:
             cnt += 1
     print(cnt)
+    n = int(sys.stdin.readline())
