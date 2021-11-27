c16bd217907b54e1c82f14860fff236ab9e17546
chaeeun <1005honeybadger@gmail.com>
2021-11-27 23:21:03 +0900

    baekjoon basic math2 1085 update and test

---
+++ 00_baekjoon/basic_math/15_RightTriangle.py
@@ -0,0 +1,25 @@
+# 4153 직각삼각형
+# # 정답 코드
+# tri = list(map(int, input().split()))
+
+# while sum(tri) != 0:
+#     if tri[0]*tri[0] + tri[1]*tri[1] == tri[2]*tri[2]:
+#         print("right")
+#     elif tri[1]*tri[1] + tri[2]*tri[2] == tri[0]*tri[0]:
+#         print("right")
+#     elif tri[0]*tri[0] + tri[2]*tri[2] == tri[1]*tri[1]:
+#         print("right")
+#     else:
+#         print("wrong")
+#     tri = list(map(int, input().split()))
+
+# 변형 코드
+tri = list(map(int, input().split()))
+
+while sum(tri) != 0:
+    tri.sort()
+    if tri[0]**2+tri[1]**2 == tri[3]**2:
+        print("right")
+    else:
+        print("wrong")
+    tri = list(map(int, input().split()))
