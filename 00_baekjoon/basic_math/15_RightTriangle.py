cd0d5ef101059bcfd4f9e597e80680e0f919ae48
chaeeun <1005honeybadger@gmail.com>
2021-11-27 23:25:04 +0900

    new code

--- 00_baekjoon/basic_math/15_RightTriangle.py
+++ 00_baekjoon/basic_math/15_RightTriangle.py
@@ -1,24 +1,25 @@
 # 4153 직각삼각형
-# # 정답 코드
-# tri = list(map(int, input().split()))
+# 정답 코드
+tri = list(map(int, input().split()))
 
-# while sum(tri) != 0:
-#     if tri[0]*tri[0] + tri[1]*tri[1] == tri[2]*tri[2]:
-#         print("right")
-#     elif tri[1]*tri[1] + tri[2]*tri[2] == tri[0]*tri[0]:
-#         print("right")
-#     elif tri[0]*tri[0] + tri[2]*tri[2] == tri[1]*tri[1]:
-#         print("right")
-#     else:
-#         print("wrong")
-#     tri = list(map(int, input().split()))
+while sum(tri) != 0:
+    if tri[0]*tri[0] + tri[1]*tri[1] == tri[2]*tri[2]:
+        print("right")
+    elif tri[1]*tri[1] + tri[2]*tri[2] == tri[0]*tri[0]:
+        print("right")
+    elif tri[0]*tri[0] + tri[2]*tri[2] == tri[1]*tri[1]:
+        print("right")
+    else:
+        print("wrong")
+    tri = list(map(int, input().split()))
 
 # 변형 코드
 tri = list(map(int, input().split()))
 
 while sum(tri) != 0:
-    tri.sort()  # 정렬 사용시 런타임 에러
-    if tri[0]**2+tri[1]**2 == tri[3]**2:
+    tri_max = max(tri)
+    tri.remove(max(tri))
+    if tri[0]**2+tri[1]**2 == tri_max**2:
         print("right")
     else:
         print("wrong")
