2969f4b37f8c1b6d66aa78765e668935d5eef9dd
chaeeun <1005honeybadger@gmail.com>
2021-11-29 19:51:00 +0900

    Add comment

--- 00_baekjoon/basic_math/17_Turret.py
+++ 00_baekjoon/basic_math/17_Turret.py
@@ -8,11 +8,11 @@ for i in range(TestCase):
 
     distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)  # 조규현과 백승환 사이의 거리
 
-    if r1 == r2 and distance == 0:
+    if r1 == r2 and distance == 0:  # r1, r2는 동일한 원
         print(-1)
-    elif abs(r1-r2) == distance or r1+r2 == distance:
+    elif abs(r1-r2) == distance or r1+r2 == distance:  # 내접, 외접
         print(1)
-    elif abs(r1-r2) < distance < r1+r2:
+    elif abs(r1-r2) < distance < r1+r2:  # 두 원은 2개의 접점을 갖는다
         print(2)
     else:
-        print(0)
+        print(0)  # 그외는 모두 접점이 없다
