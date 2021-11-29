97a3ce218d4cb0d05c60691cd4283e81a5ff2f5d
chaeeun <1005honeybadger@gmail.com>
2021-11-29 19:46:20 +0900

    baekjoon basic math2 1002

--- 00_baekjoon/basic_math/17_Turret.py
+++ 00_baekjoon/basic_math/17_Turret.py
@@ -6,10 +6,8 @@ TestCase = int(input())
 for i in range(TestCase):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
 
-    distance = int(math.sqrt((x2-x1)**2 + (y2-y1)**2))  # 조규현과 백승환 사이의 거리
-    lst = [r1, r2, distance]
-    lst_max = max(lst)
-    lst.remove(lst_max)
+    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2)  # 조규현과 백승환 사이의 거리
+
     if r1 == r2 and distance == 0:
         print(-1)
     elif abs(r1-r2) == distance or r1+r2 == distance:
