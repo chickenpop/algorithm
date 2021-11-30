273e85b6bfc92adbac5936d39718d1a3c5127467
chaeeun <1005honeybadger@gmail.com>
2021-11-30 22:37:18 +0900

    fibonacci number success, No recusion

--- 00_baekjoon/Recusion/02_FibonacciNumber.py
+++ 00_baekjoon/Recusion/02_FibonacciNumber.py
@@ -1,9 +1,14 @@
 # 10870 피보나치 수 5
-
-def Fibonacci(num):
-    return num + Fibonacci(num-1) if num > 0 else num
-
+lst = [0, 1]
 
 n = int(input())  # n 번째 피보나치 수
 
-print(Fibonacci(n))
+lst = [0, 1]
+
+if n < 2:
+    print(lst[n])
+else:
+    for i in range(2, n+1):
+        lst.append(lst[i-1] + lst[i-2])
+
+    print(lst[-1])
