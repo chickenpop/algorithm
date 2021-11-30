3cf616d9a667dbefed4c0e940bc8f6f7b8bfc967
chaeeun <1005honeybadger@gmail.com>
2021-11-30 23:05:59 +0900

    baekjoon recusion 10870 Fibonacci number 5

--- 00_baekjoon/Recusion/02_FibonacciNumber.py
+++ 00_baekjoon/Recusion/02_FibonacciNumber.py
@@ -1,14 +1,16 @@
 # 10870 피보나치 수 5
 lst = [0, 1]
 
+
+def Fibonacci(num, cnt):  # 1번째부터 적용가능한 피보나치 함수
+    cnt += 1
+    lst.append(lst[cnt-1] + lst[cnt-2])  # Fn = Fn-1 + Fn-2
+    return Fibonacci(num - 1, cnt) if num > 2 else lst[-1]
+
+
 n = int(input())  # n 번째 피보나치 수
 
-lst = [0, 1]
-
-if n < 2:
-    print(lst[n])
+if n == 0:
+    print(0)
 else:
-    for i in range(2, n+1):
-        lst.append(lst[i-1] + lst[i-2])
-
-    print(lst[-1])
+    print(Fibonacci(n, 1))
