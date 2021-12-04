89c1b607a0c66a44d85456d8d8a089c9e40ffe63
chaeeun <1005honeybadger@gmail.com>
2021-12-04 20:53:20 +0900

    baekjoon BruteForce 2231 decompose

--- 00_baekjoon/BruteForce/02_Decompose.py
+++ 00_baekjoon/BruteForce/02_Decompose.py
@@ -1,13 +1,14 @@
 # 2231 분해합
 
-n = str(input())  # 어떤 자연수, 1 <= N <= 1,000,000
+n = int(input())  # 어떤 자연수, 1 <= N <= 1,000,000
 
 
-def decompose(num):
-    sum = int(num)
-    for i in range(len(num)):
-        sum += int(num[i])
-    return sum
+for i in range(1, n+1):
+    s = sum(map(int, str(i)))  # 문자열의 각 자리를 합을 구한다
+    s += i                      # sum()+i
+    if s == n:                  # 분해합 == n 같으면 출력
+        print(i)
+        break
 
-
-print(decompose(n))
+    if i == n:                  # i == n, 마지막까지 분해합이 없으므로 0 출력
+        print(0)
