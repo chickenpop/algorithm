e139c12dc78e25499f12a0056f3e43b822d29282
chaeeun <1005honeybadger@gmail.com>
2021-12-06 21:41:45 +0900

    baekjoon BruteForce 1018 PaintChessBoard

--- 00_baekjoon/BruteForce/04_PaintChessBoard.py
+++ 00_baekjoon/BruteForce/04_PaintChessBoard.py
@@ -12,21 +12,20 @@ for i in range(n):  # 체스판을 리스트에 넣는다
 cnt_list = []  # 칠해야하는 횟수
 for i in range(n-7):
     for j in range(m-7):
-        cnt = 0  # 칠을 바꿔야하는 횟수
-        bw = chess_board[i][j]  # 첫 체스판 색깔
-        for k in range(i, i+7):
-            for l in range(j, j+7):
-                if k % 2 == 0:
-                    if l % 2 == 0 and bw != chess_board[k][l]:
-                        cnt += 1
-                    elif l % 2 == 1 and bw == chess_board[k][l]:
-                        cnt += 1
+        cnt_1 = 0  # 칠을 바꿔야하는 횟수, 시작이 'B'또는'W'
+        cnt_2 = 0  # cnt_1의 반대 경우
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if (k+l) % 2 == 0:
+                    if chess_board[k][l] != 'B':
+                        cnt_1 += 1
+                    elif chess_board[k][l] != 'W':
+                        cnt_2 += 1
                 else:
-                    if l % 2 == 0 and bw == chess_board[k][l]:
-                        cnt += 1
-                    elif l % 2 == 1 and bw != chess_board[k][l]:
-                        cnt += 1
-            if k == i+6:
-                cnt_list.append(cnt)
+                    if chess_board[k][l] != 'W':
+                        cnt_1 += 1
+                    elif chess_board[k][l] != 'B':
+                        cnt_2 += 1
+        cnt_list.append(min(cnt_1, cnt_2))
 
 print(min(cnt_list))
