d464ff0a31c908dbf2bc509d595c6a56a84dd7b9
chaeeun <1005honeybadger@gmail.com>
2021-12-06 22:16:58 +0900

    baekjoon BruteForce 1436 Number '666'

--- 00_baekjoon/BruteForce/05_Number666.py
+++ 00_baekjoon/BruteForce/05_Number666.py
@@ -1,18 +1,14 @@
 # 1436 영화감독 숌
 
 n = int(input())  # n번째 영화
-number = 666
-number_str = '666'
-cnt = 0
+number = 666  # 첫번째 종말 숫자
+cnt = 0  # n번째 종말 숫자를 찾기 위한 변수
 
 while True:
-    for i in range(len(number_str)-2):
-        if number_str[i] == '6' and number_str[i+1] == '6' and number_str[i+2] == '6':
-            cnt += 1
-
+    if '666' in str(number):  # 연속되는 666 찾기
+        cnt += 1
     if cnt == n:
-        print(number_str)
+        print(number)
         break
     else:
-        number += 1
-        number_str = str(number)
+        number += 1  # 종말숫자 + 1
