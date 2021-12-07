8813af6085128bd6e52a2ba6071eeef3f40fa10e
chaeeun <1005honeybadger@gmail.com>
2021-12-07 19:34:34 +0900

    merge, shell time out

--- 00_baekjoon/Sort/02_SortNumber02.py
+++ 00_baekjoon/Sort/02_SortNumber02.py
@@ -1,6 +1,6 @@
 # 2751 수 정렬하기 2
 
-# 병합 정렬
+# 병합 정렬 Time Out
 def mergeSort(x):
     if len(x) > 1:
         mid = len(x)//2
@@ -19,6 +19,29 @@ def mergeSort(x):
             i += 1
         x[i:] = lx[li:] if li != len(lx) else rx[ri:]
 
+# 셸 정렬 Time Out
+
+
+def gapInsertionSort(x, start, gap):
+    for target in range(start+gap, len(x), gap):
+        val = x[target]
+        i = target
+        while i > start:
+            if x[i-gap] > val:
+                x[i] = x[i-gap]
+            else:
+                break
+            i -= gap
+        x[i] = val
+
+
+def shellSort(x):
+    gap = len(x) // 2
+    while gap > 0:
+        for start in range(gap):
+            gapInsertionSort(x, start, gap)
+        gap = gap // 2
+
 
 n = int(input())  # n개의 수 입력
 lst = []  # n의 수를 저장할 리스트
@@ -26,6 +49,11 @@ for i in range(n):
     lst.append(int(input()))
 
 mergeSort(lst)
+
+for i in lst:
+    print(i)
+
+shellSort(lst)
 
 for i in lst:
     print(i)
