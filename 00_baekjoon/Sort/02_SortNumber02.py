849c888dac60bb21d289cb2a76d818e25b9769d6
chaeeun <1005honeybadger@gmail.com>
2021-12-07 19:28:33 +0900

    mergeSort

--- 00_baekjoon/Sort/02_SortNumber02.py
+++ 00_baekjoon/Sort/02_SortNumber02.py
@@ -1,12 +1,31 @@
 # 2751 수 정렬하기 2
 
+# 병합 정렬
+def mergeSort(x):
+    if len(x) > 1:
+        mid = len(x)//2
+        lx, rx = x[:mid], x[mid:]
+        mergeSort(lx)
+        mergeSort(rx)
+
+        li, ri, i = 0, 0, 0
+        while li < len(lx) and ri < len(rx):
+            if lx[li] < rx[ri]:
+                x[i] = lx[li]
+                li += 1
+            else:
+                x[i] = rx[ri]
+                ri += 1
+            i += 1
+        x[i:] = lx[li:] if li != len(lx) else rx[ri:]
+
+
 n = int(input())  # n개의 수 입력
-number = []
-
+lst = []  # n의 수를 저장할 리스트
 for i in range(n):
-    number.append(int(input()))
+    lst.append(int(input()))
 
-number.sort()
+mergeSort(lst)
 
-for i in number:
+for i in lst:
     print(i)
