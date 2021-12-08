09e5b90c95b18b49c5ff4bb60146467120955648
chaeeun <1005honeybadger@gmail.com>
2021-12-09 00:15:34 +0900

    baekjoon Sort 11650 XYsort

--- 00_baekjoon/Sort/05_XYsort.py
+++ 00_baekjoon/Sort/05_XYsort.py
@@ -2,14 +2,13 @@
 import sys
 
 n = int(input())
-xy = [[0 for _ in range(2)] for _ in range(n)]
+xy = []
 
 for i in range(n):
     x, y = map(int, sys.stdin.readline().split())
-    xy[i][0] = x
-    xy[i][1] = y
+    xy.append([x, y])  # 2차원 리스트 넣기
 
 xy.sort(key=lambda x: (x[0], x[1]))  # x, y 오름차순 정렬
 
-for i in xy:
-    print(i)
+for i in range(n):
+    print(xy[i][0], xy[i][1])
