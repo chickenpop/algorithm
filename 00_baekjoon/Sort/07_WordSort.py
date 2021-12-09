265fbb66a116d092e1cfba48b786c1203144fa4a
chaeeun <1005honeybadger@gmail.com>
2021-12-09 21:38:47 +0900

    baekjoon Sort 1181 Short code, but long time

--- 00_baekjoon/Sort/07_WordSort.py
+++ 00_baekjoon/Sort/07_WordSort.py
@@ -1,15 +1,30 @@
 # 1181 단어 정렬
-import sys
+# import sys
+
+# n = int(input())
+# word = []
+
+# for i in range(n):
+#     word.append(str(sys.stdin.readline().rstrip()))
+
+# remove_word = list(set(word))
+# remove_word.sort()
+# remove_word.sort(key=len)
+
+# for i in remove_word:
+#     print(i)
+
+# short code
 
 n = int(input())
-word = []
+word = set()
 
 for i in range(n):
-    word.append(str(sys.stdin.readline().rstrip()))
+    wo = str(input())
+    word.add((wo, len(wo)))  # set에 값 추가 add 2차원 넣기 ()
 
-remove_word = list(set(word))
-remove_word.sort()
-remove_word.sort(key=len)
+word_lst = list(word)
+word_lst.sort(key=lambda x: (x[1], x[0]))
 
-for i in remove_word:
-    print(i)
+for i in word_lst:
+    print(i[0])
