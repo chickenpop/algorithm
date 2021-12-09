0485badd05f82b2e85f198988f02dc9f86863b7d
chaeeun <1005honeybadger@gmail.com>
2021-12-09 18:57:28 +0900

    fail - runtimerror

---
+++ 00_baekjoon/Sort/06_YXsort.py
@@ -0,0 +1,14 @@
+# 11651 좌표 정렬하기 2
+import sys
+
+n = int(input())
+Yx = []
+
+for i in range(n):
+    x, y = map(int, sys.stdin.readlines().split())
+    Yx.append([x, y])
+
+Yx.sort(key=lambda x: (x[1], x[0]))
+
+for i in range(n):
+    print(Yx[i][0], Yx[i][1])
