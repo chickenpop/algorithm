c2f138a48993647450238c40608aed380fe78c25
chaeeun <1005honeybadger@gmail.com>
2021-12-10 20:08:10 +0900

    baekjoon Sort 10989 SortNumber3

--- 00_baekjoon/Sort/10_SortNumber03.py
+++ 00_baekjoon/Sort/10_SortNumber03.py
@@ -2,12 +2,13 @@
 import sys
 
 n = int(sys.stdin.readline())
-number = []
+number = [0] * 10001  # 조건은 10000이하의 수라서 +1
 
 for i in range(n):
-    number.append(sys.stdin.readline().rstrip())
+    num = int(sys.stdin.readline())
+    number[num] += 1  # 입력된 수와 같은 리스트에 +1
 
-number.sort()
-
-for i in number:
-    print(i)
+for i in range(10001):
+    if number[i] != 0:
+        for _ in range(number[i]):  # 리스트 크기만큼 출력
+            print(i)
