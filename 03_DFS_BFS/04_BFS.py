fbd13d1d46adb29d4f65cae1c35107186cbed6c6
chaeeun <1005honeybadger@gmail.com>
2021-12-13 22:25:20 +0900

    5-9 BFS

---
+++ 03_DFS_BFS/04_BFS.py
@@ -0,0 +1,37 @@
+# 5-9 BFS
+from collections import deque
+
+
+def bfs(graph, start, visited):
+    # 큐 구현을 위한 deque
+    queue = deque([start])
+    # 현재 노드 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 방문하지 않은 인접한 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [2, 6, 8],
+    [7],
+    [2, 6, 8],
+    [1, 7],
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
