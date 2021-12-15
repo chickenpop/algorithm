3bb64dc7fa062edc33f1164e9013ae7c95e6040a
chaeeun <1005honeybadger@gmail.com>
2021-12-15 19:48:31 +0900

    beakjoon Backtracking 15652 NandM04

--- 00_baekjoon/Backtracking/04_NandM04.py
+++ 00_baekjoon/Backtracking/04_NandM04.py
@@ -5,16 +5,15 @@ n, m = map(int, input().split())
 lst = []
 
 
-def NnM4():
+def NnM4(k):
     if len(lst) == m:
         print(' '.join(map(str, lst)))
         return
 
-    for i in range(1, n+1):
-        if sum(lst) <= i:
-            lst.append(i)
-            NnM4()
-            lst.pop()
+    for i in range(k, n+1):
+        lst.append(i)
+        NnM4(i)
+        lst.pop()
 
 
-NnM4()
+NnM4(1)
