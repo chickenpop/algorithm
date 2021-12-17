ab4e6d5e447b5fa195081565ad2bdb2ac1ec6c28
chaeeun <1005honeybadger@gmail.com>
2021-12-17 22:19:21 +0900

    baekjoon DFS+BFS 1260 DFS and BFS

--- 00_baekjoon/DFS+BFS/DFSandBFS.py
+++ 00_baekjoon/DFS+BFS/DFSandBFS.py
@@ -3,10 +3,42 @@ from collections import deque
 
 n, m, v = map(int, input().split())  # 정점, 간선, 탐색 시작번호
 
-graph = []
+graph = [[] for _ in range(n+1)]
 
 for i in range(m):
     x, y = map(int, input().split())
-    graph.append([x, y])
+    graph[x].append(y)
+    graph[y].append(x)
 
-# 아 기본부터 연습해야겠어... 벌써 구조가 기억이 안남
+for i in range(1, n+1):
+    graph[i].sort()
+
+
+dfs_visited = [False] * (n+1)
+bfs_visited = [False] * (n+1)
+
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+dfs(graph, v, dfs_visited)
+print()
+bfs(graph, v, bfs_visited)
