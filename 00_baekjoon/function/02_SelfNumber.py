965383cd7720f22491faf4d57ab2b40cc1f622df
chaeeun <1005honeybadger@gmail.com>
2021-12-21 20:26:15 +0900

    beakjoon funcion 4673 SelfNumber

--- 00_baekjoon/function/02_SelfNumber.py
+++ 00_baekjoon/function/02_SelfNumber.py
@@ -1,14 +1,15 @@
 # 4673 셀프 넘버
 
-def SelfNumber(num):
-    for i in range(1, num+1):
-        s = sum(map(int, str(i))) + i
-        if s == num:
-            break
-
-        if i == num:
-            print(num)
+def d(num):
+    n = sum(map(int, str(num))) + num
+    return n
 
 
-for i in range(10001):
-    SelfNumber(i)
+NoSelfNum = set()
+
+for i in range(1, 10001):
+    NoSelfNum.add(d(i))
+
+for i in range(1, 10001):
+    if i not in NoSelfNum:
+        print(i)
