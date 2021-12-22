5bc68ad630101445233ddcdcce250b2fb45f97ff
chaeeun <1005honeybadger@gmail.com>
2021-12-22 21:21:25 +0900

    baekjoon Function 1065 한수

--- 00_baekjoon/function/03_ArithmeticSequence.py
+++ 00_baekjoon/function/03_ArithmeticSequence.py
@@ -1,20 +1,24 @@
 # 1065 한수
 
-n = int(input())  # 1000이하의 n
-
-cnt = 0
-for i in range(1, n+1):
-    if i < 10:
-        cnt += 1
-        continue
-    lst = list(map(int, str(i)))
+def seq(n):
+    if n < 10:
+        return 1
+    lst = list(map(int, str(n)))
     a = lst[0]
     d = lst[1] - a
     for j in range(len(lst)):
         if (a + d*(j)) != lst[j]:
             break
         if j == len(lst)-1:
-            cnt += 1
+            return 1
+
+
+n = int(input())  # 1000이하의 n
+
+cnt = 0
+for i in range(1, n+1):
+    if seq(i) == True:
+        cnt += 1
 
 
 print(cnt)
