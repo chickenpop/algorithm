d1bbaba0bfed941bbacf47a9f4a2dc4365fedfcd
chaeeun <1005honeybadger@gmail.com>
2021-12-29 23:12:06 +0900

    fail- no function, sort, timeout

--- 00_baekjoon/array/01_MaxMin.py
+++ 00_baekjoon/array/01_MaxMin.py
@@ -5,3 +5,16 @@ n = int(input())
 lst = list(map(int, input().split()))
 
 print(min(lst), max(lst))
+
+# 시간초과
+n = int(input())
+
+lst = list(map(int, input().split()))
+
+for i in range(n):
+    index = i
+    for i in range(len(lst)):
+        if lst[index] > lst[i]:
+            lst[index], lst[i] = lst[i], lst[index]
+
+print(lst[-1], lst[0])
