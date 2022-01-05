6ba79be29bb264a858a34f2d0568a3f34517e96e
chaeeun <1005honeybadger@gmail.com>
2022-01-06 00:27:16 +0900

    fail - part end

--- 00_baekjoon/String/04_word.py
+++ 00_baekjoon/String/04_word.py
@@ -1,7 +1,23 @@
 # 1157 단어 공부
 
 word = list(str(input()))
-
-# .count() 단어 개수
+word_upper = []
+abc = [0] * 27
+abc[26] = -1
 for i in word:
-    print(word.count(i))
+    word_upper.append(i.upper())
+
+for i in word_upper:
+    index = ord(i) - 65
+    abc[index] += 1
+
+word_max = abc[0]
+for i in abc:
+    if i != abc[0] and i > word_max:
+        word_max = i
+    elif i != abc[0] and i == word_max:
+        print("?")
+        break
+    if i == abc[-1]:
+        print(chr(abc.index(word_max)+65))
+        break
