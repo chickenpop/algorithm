b410f43e28275024abe0df85c6f409344b1b3689
chaeeun <1005honeybadger@gmail.com>
2022-01-06 00:54:42 +0900

    fail - not cause

--- 00_baekjoon/String/04_word.py
+++ 00_baekjoon/String/04_word.py
@@ -12,12 +12,14 @@ for i in word_upper:
     abc[index] += 1
 
 word_max = abc[0]
-for i in abc:
-    if i != abc[0] and i > word_max:
-        word_max = i
-    elif i != abc[0] and i == word_max:
+
+for i in range(1, len(abc)+1):
+    if abc[i] > word_max:
+        word_max = abc[i]
+    elif abc[i] == word_max and word_max > 0:
         print("?")
         break
-    if i == abc[-1]:
+
+    if abc[i] == abc[-1]:
         print(chr(abc.index(word_max)+65))
         break
