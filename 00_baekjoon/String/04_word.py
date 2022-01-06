d187e247304b12f2886eedaa41ef5bc66458d0d2
chaeeun <1005honeybadger@gmail.com>
2022-01-06 22:26:15 +0900

    baekjoon String 1157 단어공부

--- 00_baekjoon/String/04_word.py
+++ 00_baekjoon/String/04_word.py
@@ -1,25 +1,14 @@
 # 1157 단어 공부
 
-word = list(str(input()))
-word_upper = []
-abc = [0] * 27
-abc[26] = -1
-for i in word:
-    word_upper.append(i.upper())
+word = input().lower()
+word_list_set = list(set(word))
+word_cnt = []
 
-for i in word_upper:
-    index = ord(i) - 65
-    abc[index] += 1
+for i in word_list_set:
+    cnt = word.count(i)
+    word_cnt.append(cnt)
 
-word_max = abc[0]
-
-for i in range(1, len(abc)+1):
-    if abc[i] > word_max:
-        word_max = abc[i]
-    elif abc[i] == word_max and word_max > 0:
-        print("?")
-        break
-
-    if abc[i] == abc[-1]:
-        print(chr(abc.index(word_max)+65))
-        break
+if word_cnt.count(max(word_cnt)) >= 2:
+    print("?")
+else:
+    print(word_list_set[(word_cnt.index(max(word_cnt)))].upper())
