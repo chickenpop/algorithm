216cb8f34614bd09ad46c160556354b6ac0d4878
chaeeun <1005honeybadger@gmail.com>
2022-01-14 23:18:25 +0900

    time over

--- 00_baekjoon/Stack/01_Stack.py
+++ 00_baekjoon/Stack/01_Stack.py
@@ -9,14 +9,14 @@ for i in range(n):
     if order[0] == 'push':  # 스택에 정수를 넣는다.
         stack.append(order[1])
     elif order[0] == 'pop':  # 스택에서 가장 위에 있는 정수 뺀다. 없으면 -1
-        if len(order) > 0:
-            print(stack.pop())
-        else:
+        if len(stack) == 0:
             print(-1)
+        else:
+            print(stack.pop())
     elif order[0] == 'size':  # 스택의 정수 갯수
-        print(len(order))
+        print(len(stack))
     elif order[0] == 'empty':  # 스택이 비어 있으면 1, 아니면 0
-        if len(order) == 0:
+        if len(stack) == 0:
             print(1)
         else:
             print(0)
