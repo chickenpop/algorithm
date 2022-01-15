c08fca927b6e548b243c8031a4eafb89296cdd6a
chaeeun <1005honeybadger@gmail.com>
2022-01-16 00:06:49 +0900

    fail-no cauese

--- 00_baekjoon/Stack/03_VPS.py
+++ 00_baekjoon/Stack/03_VPS.py
@@ -1,17 +1,15 @@
 # 9012 괄호
 
-stack = []
 n = int(input())  # 테스트 케이스 수
 for i in range(n):
+    stack = []
     vps = list(map(str, input()))
-    right = 0
-    left = 0
     for i in range(len(vps)):
         if vps[i] == "(":
-            left += 1
+            stack.append(1)
         else:
-            right += 1
-    if right == left:
+            stack.append(-1)
+    if sum(stack) == 0:
         print("YES")
     else:
         print("NO")
