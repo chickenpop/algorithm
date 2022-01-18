da843f88b67434e5e9796e894a8e5319c065cdc8
chaeeun <1005honeybadger@gmail.com>
2022-01-19 00:09:05 +0900

    baekjoon Stack 4949 balancedworld

--- 00_baekjoon/Stack/04_BalancedWorld.py
+++ 00_baekjoon/Stack/04_BalancedWorld.py
@@ -4,16 +4,16 @@ import sys
 while True:
     stack = []
     flag = True
-    Input = list(map(str, sys.stdin.readline()))
+    Input = sys.stdin.readline().rstrip()
     if Input == ".":
         break
     for i in Input:
-        if i == "(" or i == "[":
+        if i == "(" or i == "[":  # 열린 기호 push
             stack.append(i)
-        elif i == ")":
+        elif i == ")":  # 기호 일치하면 pop
             if stack and stack[-1] == "(":
                 stack.pop()
-            else:
+            else:  # 기호 불일치 break
                 flag = False
                 break
         elif i == "]":
@@ -22,7 +22,7 @@ while True:
             else:
                 flag = False
                 break
-    if flag == True:
+    if flag == True and not stack:  # 스택도 비어 있어야한다
         print("yes")
     else:
         print("no")
