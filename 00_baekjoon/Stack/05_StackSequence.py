5b2c738d74c59f716af6fd4bd71c81149d4c2da0
chaeeun <1005honeybadger@gmail.com>
2022-01-20 01:52:29 +0900

    beakjoon Stack 1874 StackSequence

--- 00_baekjoon/Stack/05_StackSequence.py
+++ 00_baekjoon/Stack/05_StackSequence.py
@@ -4,18 +4,25 @@ import sys
 n = int(sys.stdin.readline())
 stack = []
 start = 1
+result = []
+flag = True
 for i in range(n):
     Input = int(sys.stdin.readline())
     while start <= Input:
         stack.append(start)
-        print("+")
+        result.append("+")
         start += 1
 
     if stack[-1] == Input:
         stack.pop()
-        print("-")
-    elif len(stack) == 0:
-        break
+        result.append("-")
     else:
+        flag = False
         print("NO")
         break
+    if flag == False:
+        break
+
+if flag == True:
+    for i in result:
+        print(i)
