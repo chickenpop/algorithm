48ca367f3f2fe7d0d37e45bc39d613894e4213ee
chaeeun <1005honeybadger@gmail.com>
2022-01-20 01:42:04 +0900

    fail - print over

--- 00_baekjoon/Stack/05_StackSequence.py
+++ 00_baekjoon/Stack/05_StackSequence.py
@@ -1,28 +1,21 @@
 # 1874 스택 수열
 import sys
-stack = []
+
 n = int(sys.stdin.readline())
-flag = True
+stack = []
 start = 1
 for i in range(n):
     Input = int(sys.stdin.readline())
-    for i in range(start, n+2):
-        if i == Input and stack and i == stack[-1]:
-            stack.pop()
-            print("-")
-            start = i
-            break
-        elif Input in stack and stack[-1] == Input:
-            stack.pop()
-            print("-")
-            start = i
-            break
-        elif Input in stack and stack[-1] != Input:
-            print("NO")
-            flag = False
-            break
-        else:
-            stack.append(i)
-            print("+")
-    if flag == False:
+    while start <= Input:
+        stack.append(start)
+        print("+")
+        start += 1
+
+    if stack[-1] == Input:
+        stack.pop()
+        print("-")
+    elif len(stack) == 0:
+        break
+    else:
+        print("NO")
         break
