c0827cf11fa054bb11f2a0f0835cc22b91169359
chaeeun <1005honeybadger@gmail.com>
2022-01-22 01:13:12 +0900

    beakjoon Stack 17298 NGE

--- 00_baekjoon/Stack/06_NGE.py
+++ 00_baekjoon/Stack/06_NGE.py
@@ -2,18 +2,13 @@
 import sys
 n = int(sys.stdin.readline())  # 수열의 크기
 stack = []
-stack = list(map(int, sys.stdin.readline().split()))
-stack.append(-1)  # -1
-result = []
+result = [-1] * n
+stk = list(map(int, sys.stdin.readline().split()))
 
 for i in range(n):
-    Nge = stack[i]
-    for j in range(i+1, n+1):
-        if Nge < stack[j]:
-            result.append(stack[j])
-            break
-        elif j == n:
-            result.append(stack[j])  # -1 출력
+    while stack and stk[stack[-1]] < stk[i]:
+        result[stack[-1]] = stk[i]
+        stack.pop()
+    stack.append(i)
 
-for i in result:
-    print(i, end=" ")
+print(*result)
