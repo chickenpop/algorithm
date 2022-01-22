919b54d8972cc2a68120270b39f388a76ea0a50f
chaeeun <1005honeybadger@gmail.com>
2022-01-22 23:55:11 +0900

    beakjoon Queue 18258 Queue 2

--- 00_baekjoon/Queue/01_Queue2.py
+++ 00_baekjoon/Queue/01_Queue2.py
@@ -2,6 +2,7 @@
 
 from collections import deque  # 데이터를 양방향에서 추가하거나 제거가능한 자료구조
 import sys
+
 # queue = deque([1, 2, 3])  # 생성
 # print("생성{}".format(queue))
 # queue.append(4)  # 삽입
@@ -13,33 +14,31 @@ import sys
 
 n = int(sys.stdin.readline())  # 명령 총 횟수
 queue = deque()  # 빈 큐 생성
-list = []  # 결과 출력 리스트
+
 for i in range(n):
     Input = list(map(str, sys.stdin.readline().split()))
     if Input[0] == "push":
         queue.append(Input[1])
     elif Input[0] == "pop":
         if len(queue) == 0:
-            list.append(-1)
+            print(-1)
         else:
+            print(queue[0])
             queue.popleft()
     elif Input[0] == "size":
-        list.append(len(queue))
+        print(len(queue))
     elif Input[0] == "empty":
         if len(queue) == 0:
-            list.append(1)
+            print(1)
         else:
-            list.append(0)
+            print(0)
     elif Input[0] == "front":
-        if len(queue) == 0:
-            list.append(-1)
+        if queue:
+            print(queue[0])
         else:
-            list.append(queue[0])
+            print(-1)
     elif Input[0] == "back":
-        if len(queue) == 0:
-            list.append(-1)
+        if queue:
+            print(queue[-1])
         else:
-            list.append(queue[-1])
-
-for i in list:
-    print(i)
+            print(-1)
