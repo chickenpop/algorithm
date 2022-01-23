8a9bad05df1dd5f9d6dcdc4982fb8e7207a32293
chaeeun <1005honeybadger@gmail.com>
2022-01-23 23:11:33 +0900

    fail- print over

---
+++ 00_baekjoon/Queue/02_Card2.py
@@ -0,0 +1,16 @@
+# 2164 카드 2
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline())
+que = deque([i for i in range(1, n+1)])  # queue 사용하면 import queue 자동으로 입력됨
+
+while que:
+    if len(que) == 1:
+        print(que[0])
+        break
+    else:
+        que.popleft()
+        MoveRightCard = que.popleft()
+        que.append(MoveRightCard)
+        print(que)
