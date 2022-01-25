866cbd527de17863849e8bf2c89ddafeb4c56447
chickenpop <dkzktldkthsu@naver.com>
2022-01-25 21:58:32 +0900

    input change, fail - range of list

--- 00_baekjoon/Queue/04_PrintQueue.py
+++ 00_baekjoon/Queue/04_PrintQueue.py
@@ -5,5 +5,20 @@ from collections import deque
 TestCase = int(sys.stdin.readline())
 for i in range(TestCase):
   n, m = map(int, sys.stdin.readline().split()) #문서갯수, 몇번째로 인쇄되는지 궁금한 문서 
-  que = sys.stdin.readline().strip() # 공백문자 남아있음
-  queue = deque(que.replace(" ", "")) # 공백제거 큐에 입력
+  que = deque(sys.stdin.readline().split()) # 공백문자 남아있음 # 공백제거 큐에 입력  
+  m_move = 0
+  while que:
+    MaxQue = max(que)
+    if MaxQue == que[0]:
+      m_move += 1
+      if MaxQue == que[m]:
+        print(m_move)
+        break
+      que.pop()
+    else:
+      data = que.popleft()
+      que.append(data)
+      if m == 0:
+        m = len(que) - 1
+      else:
+        m -= 1
