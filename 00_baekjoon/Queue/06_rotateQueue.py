4a0fdc9300b417a4550708df36743f060a0dd2d9
chaeeun <1005honeybadger@gmail.com>
2022-02-10 23:24:07 +0900

    fail - rotate queue

--- 00_baekjoon/Queue/06_rotateQueue.py
+++ 00_baekjoon/Queue/06_rotateQueue.py
@@ -2,7 +2,30 @@
 from collections import deque
 import sys
 
-n, m = map(int, sys.stdin.readline())
-lst = list(map(int, sys.stdin.readline().split()))
+# n = 큐의 크기, m = 뽑아내려는 개수
+n, m = map(int, sys.stdin.readline().split())
+lst = list(map(int, sys.stdin.readline().split()))  # 뽑아내려는 큐의 위치
 
-# 디버깅 오류 발생 진행 할 수 없음
+
+# n 크기의 큐 생성
+que = deque()
+for i in range(1, n+1):
+    que.append(int(i))
+
+cnt = 0  # 2, 3번 수행
+for i in lst:
+    while True:
+        if que[0] == i:
+            que.popleft()
+            break
+        else:
+            if que.index(i) < len(que)/2:  # 인덱스 위치가 길이의 절반보다 작으면 맨앞의 수를 맨뒤로 이동
+                while que[0] != i:
+                    que.rotate(1)
+                    cnt += 1
+            else:  # 인덱스 위치가 길이의 절반보다 크면 맨뒤에 수를 맨앞으로
+                while que[0] != i:  # i이랑 같지 않으면 계속 반복
+                    que.rotate(-1)
+                    cnt += 1
+
+print(cnt)
